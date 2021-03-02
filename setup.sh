6e07fd68235160656e5645ab1021036ba8f0c665
chatelao <olivier.chatelain@gmail.com>
2021-03-02 07:54:31 +0100

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+sudo apt-get update
+# sudo apt-get install -y \
+#                 gcc \
+#                 git \
+#                 cmake \
+#                 python \
+#                 gcc-arm-none-eabi \
+#                 binutils-arm-none-eabi \
+#                 openocd
+
+packagelist=(
+    gcc
+    git
+    cmake
+    python
+    gcc-arm-none-eabi
+    binutils-arm-none-eabi
+    python3-yt
+    python3-pip
+    openocd
+)
+sudo apt-get install ${packagelist[@]}
+
+#
+# Install GDB for ARM
+#
+# wget http://archive.ubuntu.com/ubuntu/pool/main/r/readline6/libreadline6_6.3-8ubuntu2_amd64.deb
+# sudo dpkg -i libreadline6_6.3-8ubuntu2_amd64.deb
+
+# wget http://archive.ubuntu.com/ubuntu/pool/universe/g/gdb-arm-none-eabi/gdb-arm-none-eabi_7.10-1ubuntu3+9_amd64.deb
+# sudo dpkg -i gdb-arm-none-eabi_7.10-1ubuntu3+9_amd64.deb
+# sudo apt --fix-broken -y install
+
+# git clone https://github.com/lancaster-university/microbit-v2-samples
+
+# cd microbit-v2-samples
+# python build.py
