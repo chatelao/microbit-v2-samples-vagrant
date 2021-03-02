ad5dbabe9c9a1d277147c52a42296b721475702b
chatelao <olivier.chatelain@gmail.com>
2021-03-02 08:05:31 +0100

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -21,19 +21,22 @@ packagelist=(
     python3-pip
     openocd
 )
-sudo apt-get install ${packagelist[@]}
+sudo apt-get install -y ${packagelist[@]}
 
 #
 # Install GDB for ARM
 #
-# wget http://archive.ubuntu.com/ubuntu/pool/main/r/readline6/libreadline6_6.3-8ubuntu2_amd64.deb
-# sudo dpkg -i libreadline6_6.3-8ubuntu2_amd64.deb
+wget http://archive.ubuntu.com/ubuntu/pool/main/r/readline6/libreadline6_6.3-8ubuntu2_amd64.deb
+sudo dpkg -i libreadline6_6.3-8ubuntu2_amd64.deb
 
-# wget http://archive.ubuntu.com/ubuntu/pool/universe/g/gdb-arm-none-eabi/gdb-arm-none-eabi_7.10-1ubuntu3+9_amd64.deb
-# sudo dpkg -i gdb-arm-none-eabi_7.10-1ubuntu3+9_amd64.deb
-# sudo apt --fix-broken -y install
+wget http://archive.ubuntu.com/ubuntu/pool/universe/g/gdb-arm-none-eabi/gdb-arm-none-eabi_7.10-1ubuntu3+9_amd64.deb
+sudo dpkg -i gdb-arm-none-eabi_7.10-1ubuntu3+9_amd64.deb
+sudo apt --fix-broken -y install
 
-# git clone https://github.com/lancaster-university/microbit-v2-samples
+#
+# Fetch and build the microbit samples
+#
+git clone https://github.com/lancaster-university/microbit-v2-samples
 
-# cd microbit-v2-samples
-# python build.py
+cd microbit-v2-samples
+python build.py
